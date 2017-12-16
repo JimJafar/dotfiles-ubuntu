ee11be79fa534a962f8f5f2009843711fe8605e3
Jim Sangwine <jim@sangwine.net>
2017-12-16 13:38:01 +0800

    Fix motd and set zsh as default

--- install/install.sh
+++ install/install.sh
@@ -1,23 +1,43 @@
 #!/usr/bin/env bash
 
+echo "git config"
+
 if [ -f ~/.gitconfig ]; then
   mv ~/.gitconfig ~/.gitconfig-pre-dotfiles-bak
 fi
 ln -s ~/dotfiles-ubuntu/config/.gitconfig ~/.gitconfig
+
+echo "bash aliases"
 
 if [ -f ~/.bash_aliases ]; then
   mv ~/.bash_aliases ~/.bash_aliases-pre-dotfiles-bak
 fi
 ln -s ~/dotfiles-ubuntu/config/.bash_aliases ~/.bash_aliases
 
+echo "zshrc"
+
 if [ -f ~/.zshrc ]; then
   mv ~/.zshrc ~/.zshrc-pre-dotfiles-bak
 fi
 ln -s ~/dotfiles-ubuntu/config/.zshrc ~/.zshrc
 
-if [ -f ~/.motd.d ]; then
-  mv ~/.zshrc ~/.motd.d-pre-dotfiles-bak
+echo "motd"
+
+if [ -d ~/.motd.d ]; then
+  if [ ! -L ~/.motd.d ]; then
+    mv ~/.motd.d ~/.motd.d-pre-dotfiles-bak
+  else
+    rm ~/.motd.d
+  fi
 fi
-ln -s ~/dotfiles-ubuntu/config/.motd.d ~/.motd.d
+ln -s ~/dotfiles-ubuntu/config/.motd.d ~/
+
+echo "file templates"
 
 sh ~/dotfiles-ubuntu/install/templates.sh
+
+echo "changing default shell to zsh"
+sudo chsh -s /bin/zsh
+sudo chsh -s /bin/zsh jim
+
+echo "DONE"
